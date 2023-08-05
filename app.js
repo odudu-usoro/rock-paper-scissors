d3ff6105e94b1f25bc07a316b0d914dab001c986
Oduduabasi Usoro <yakndarausoro1@gmail.com>
2023-08-05 13:50:01 +0100

    Fixed error with id in the span tag Added code to display user choice

--- app.js
+++ app.js
@@ -1,54 +1,112 @@
-let playerScore = 0;
-let computerScore = 0;
+const computerChoice = document.getElementById("computerChoice");
+const playerChoice = document.getElementById("playerChoice");
+const result = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
+let userChoice = "";
 
-function computerChoice() {
-    const randomChoice = Math.floor(Math.random() * 3);
-    if (randomChoice === 0) {
-        return "rock";
-    }else if (randomChoice === 1){
-        return "paper";
-    }else {
-        return "scissors";
-    }
-}
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener("click", (e) => {
+    userChoice = e.target.id
+    playerChoice.innerHTML = userChoice
+}))
 
-function playRound(playerChoice, randomChoice) {
-    if (playerChoice === randomChoice) {
-        return ("It's a Tie");
-    } else if (
-        (playerChoice === "paper" && randomChoice === "rock")||
-        (playerChoice === "rock" && randomChoice === "scissors")||
-        (playerChoice === "scissors" && randomChoice === "paper")
-    ) {
-        return "Player Wins";
-    } else {
-        return "Computer Wins";
-    }
-}
 
-function game() {
-    playerScore = 0;
-    computerScore = 0;
 
-    // for(let round = 1; round <= 5; round ++){
-    //     const playerChoice = window.prompt("Enter your Choice", "rock");
-    //     const randomChoice = computerChoice();
-    //     const result = playRound(playerChoice, randomChoice);
-    //     console.log(result);
 
-        if (result === "player wins") {
-            playerScore++;
-        } else if (result === "computer wins"){
-            computerScore++;
-        }    
-    }
-    
-    if (playerScore > computerScore){
-        console.log("player wins the game");
-    } else if (playerScore < computerScore){
-        console.log("computer wins the game");
-    } else {
-        console.log("It's a Tie nobody wins");
-    }
-}
-game();
+
+
+
+// let playerScore = 0;
+// let computerScore = 0;
+
+// function computerChoice() {
+//     const randomChoice = Math.floor(Math.random() * 3);
+//     if (randomChoice === 0) {
+//         return "rock";
+//     }else if (randomChoice === 1){
+//         return "paper";
+//     }else {
+//         return "scissors";
+//     }
+// }
+
+// function playRound(playerChoice, randomChoice) {
+//     if (playerChoice === randomChoice) {
+//         return ("It's a Tie");
+//     } else if (
+//         (playerChoice === "paper" && randomChoice === "rock")||
+//         (playerChoice === "rock" && randomChoice === "scissors")||
+//         (playerChoice === "scissors" && randomChoice === "paper")
+//     ) {
+//         return "Player Wins";
+//     } else {
+//         return "Computer Wins";
+//     }
+// }
+
+// function game() {
+//     playerScore = 0;
+//     computerScore = 0;
+
+//     let playerScore = 0;
+//     let computerScore = 0;
+
+//     function computerChoice() {
+//         const randomChoice = Math.floor(Math.random() * 3);
+//         if (randomChoice === 0) {
+//             return "rock";
+//         }else if (randomChoice === 1){
+//             return "paper";
+//         }else {
+//             return "scissors";
+//         }
+//     }
+
+//     function playRound(playerChoice, randomChoice) {
+//         if (playerChoice === randomChoice) {
+//             return ("It's a Tie");
+//         } else if (
+//             (playerChoice === "paper" && randomChoice === "rock")||
+//             (playerChoice === "rock" && randomChoice === "scissors")||
+//             (playerChoice === "scissors" && randomChoice === "paper")
+//         ) {
+//             return "Player Wins";
+//         } else {
+//             return "Computer Wins";
+//         }
+//     }
+
+//     function game() {
+//         playerScore = 0;
+//         computerScore = 0;
+
+//         // for(let round = 1; round <= 5; round ++){
+//         //     const playerChoice = window.prompt("Enter your Choice", "rock");
+//         //     const randomChoice = computerChoice();
+//         //     const result = playRound(playerChoice, randomChoice);
+//         //     console.log(result);
+
+//         //     if (result === "player wins") {
+//         //         playerScore++;
+//         //     } else if (result === "computer wins"){
+//         //         computerScore++;
+//         //     }    
+//         // }
+        
+//         if (playerScore > computerScore){
+//             console.log("player wins the game");
+//         } else if (playerScore < computerScore){
+//             console.log("computer wins the game");
+//         } else {
+//             console.log("It's a Tie nobody wins");
+//         }
+//     }
+//     game(); 
+//     if (playerScore > computerScore){
+//         console.log("player wins the game");
+//     } else if (playerScore < computerScore){
+//         console.log("computer wins the game");
+//     } else {
+//         console.log("It's a Tie nobody wins");
+//     }
+// }
+// game();
